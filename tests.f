f0335ca6c50ae33531948f4ba5a1f845ac8ef436
alban read <albanread@googlemail.com>
2021-11-27 10:01:01 +0000

    Added FIB revealing  the  interpreter is slow.

--- tests.f
+++ tests.f
@@ -5,3 +5,20 @@
 : RECT 255 0 DO I 16 MOD 0= IF  CR  THEN .' *' LOOP ;
 
 : TABLE  CR 11 1 DO  11 1 DO  I J *  .  SPACE LOOP CR LOOP ;
+
+: FIB  
+  DUP 2 < IF
+   DROP 1
+  ELSE
+   DUP
+   1- FIB
+   SWAP 2 - FIB
+   +
+ THEN ;
+
+ : run-fib 
+ TICKS 
+    25 0 DO 
+        34 FIB DROP 
+    LOOP 
+ TICKS SWAP - TPMS / . ;
