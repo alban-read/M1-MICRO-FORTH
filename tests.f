db4b50313ce306a3533cd1722898af498b8f1194
alban read <albanread@googlemail.com>
2021-11-28 08:49:35 +0000

    Add comment words

--- tests.f
+++ tests.f
@@ -8,45 +8,7 @@
 
 
 
- : run-fib 
- TICKS 
-    25 0 DO 
-        34 FIB DROP 
-    LOOP 
- TICKS SWAP - TPMS / . ;
 
 
-: FIB  
-  DUP 2 < IF
-   DROP 1
-  ELSE
-   DUP
-   1- FIB
-   SWAP 2 - FIB
-   +
- THEN ;
-
-
-
-: ul-fib-1
-  34 FIB DROP 
-  34 FIB DROP 
-  34 FIB DROP 
-  34 FIB DROP 
-  34 FIB DROP 
- ;
-
- : ul-fib-2
- ul-fib-1
- ul-fib-1
- ul-fib-1
- ul-fib-1
- ul-fib-1
- ;
-
-
-
- : run-ulfib 
-  TICKS  ul-fib-2  TICKS SWAP - TPMS / . ;
 
 
